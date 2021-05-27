3b674aa81fded958fc0c00871acb1f177f5aa920
thejoshcooter <thejoshcooter@gmail.com>
2021-05-27 14:19:13 -0500

    finish setting up basis for nav

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
 import {
   View,
   Text
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import DeckList from './views/DeckList'
+import IndividualDeck from './views/IndividualDeck'
+import NewDeck from './views/NewDeck'
+import NewQuestion from './views/NewQuestion'
+import Quiz from './views/Quiz'
+
+// create navigation
+const PrimaryStack = createStackNavigator()
 
 const App = () => {
   return (
     <>
     <NavigationContainer>
-      <View>
-        <Text>hello world</Text>
-      </View>
+      <PrimaryStack.Navigator initialRouteName='DeckList'>
+        <PrimaryStack.Screen name='DeckList' component={DeckList} options={{ title: 'Home' }} />
+        <PrimaryStack.Screen name='Individual Deck' component={IndividualDeck} />
+        <PrimaryStack.Screen name='NewDeck' component={NewDeck} />
+        <PrimaryStack.Screen name='NewQuestion' component={NewQuestion} />
+        <PrimaryStack.Screen name='Quiz' component={Quiz} />
+      </PrimaryStack.Navigator>
     </NavigationContainer>
     </>
   )
